cd0b23b29e7e5a39c1f9e77713b7d1b304c04116
Tess <tere0506@stud.kea.dk>
2022-08-31 11:11:35 +0200

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", countingArray);
+
+let counter = 0;
+const anArray = [];
+
+function countingArray() {
+  if (counter < 50) {
+    console.log(counter);
+    anArray.unshift(counter);
+    console.log(anArray);
+    if (anArray.length > 8) {
+      anArray.pop();
+    }
+    counter += 1;
+    setTimeout(countingArray, 1000);
+  }
+}
